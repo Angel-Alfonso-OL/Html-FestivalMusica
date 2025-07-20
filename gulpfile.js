3ff55dfdaed7111e328c2d0b3ce6da1a2e0b7a6d
Angel Orozco <angelalfonsool20@gmail.com>
2025-07-20 14:51:02 -0600

    Automatizando sass con gulp.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import gulpSass from "gulp-sass"
+import * as dartSass from "sass"
+import { src, dest, watch } from "gulp"
+
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    src("src/scss/app.scss")
+    .pipe(sass())
+    .pipe(dest("build/css/app.css"))
+
+    done();
+}
+
+export function dev(){
+    watch("src/scss/app.scss", css);
+}
