059825ddf4a79f8c51d8223a66986425fd084453
Angel Orozco <angelalfonsool20@gmail.com>
2025-08-01 07:10:57 -0600

    Agregando js para la creacion de la galeria de imagenes.

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,12 @@
-alert("hola");
+document.addEventListener("DOMContentLoaded", crearGaleria);
+
+function crearGaleria(){
+    const galeria = document.querySelector(".galeria-imagenes");
+    for(let i = 1; i <= 16; i++){
+        const img = document.createElement("IMG");
+        img.src = '/src/gallery/full/'+i+'.jpg';
+        img.alt = "Imagen Musica";
+        galeria.appendChild(img);
+
+    }
+}
