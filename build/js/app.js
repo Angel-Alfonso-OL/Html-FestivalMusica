9c3edeef66691fd37b7b1abf313660cf3b4f64a9
Angel Orozco <angelalfonsool20@gmail.com>
2025-08-01 08:16:12 -0600

    Agregando los grid en galarias para cada uno de los dispositivos.

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", crearGaleria);
 
 function crearGaleria(){
+    
     const galeria = document.querySelector(".galeria-imagenes");
-    for(let i = 1; i <= 16; i++){
+    const CANTIDAD_IMAGENES = 16;
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
         const img = document.createElement("IMG");
         img.src = '/src/gallery/full/'+i+'.jpg';
         img.alt = "Imagen Musica";
