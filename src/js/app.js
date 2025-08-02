65288b77b12dbaf8a11110950e49d5d5e5feaf9d
Angel Orozco <angelalfonsool20@gmail.com>
2025-08-02 10:17:08 -0600

    Agregando configuraciones de modal.

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,49 @@
 document.addEventListener("DOMContentLoaded", crearGaleria);
 
-function crearGaleria(){
-    
+function crearGaleria() {
+
     const galeria = document.querySelector(".galeria-imagenes");
     const CANTIDAD_IMAGENES = 16;
-    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
+    for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
         const img = document.createElement("IMG");
-        img.src = '/src/gallery/full/'+i+'.jpg';
+        img.src = '/src/gallery/full/' + i + '.jpg';
         img.alt = "Imagen Musica";
         galeria.appendChild(img);
 
-        img.onclick = function(){
+        img.onclick = function () {
             mostrarImagen(i);
         }
 
     }
 }
 
-function mostrarImagen(i){
+function mostrarImagen(i) {
+
+    const img = document.createElement("IMG");
+    img.src = '/src/gallery/full/' + i + '.jpg';
+    img.alt = "Imagen Musica";
+
+
     const modal = document.createElement("DIV");
     modal.classList.add("modal");
+
+    modal.appendChild(img);
 
     modal.onclick = cerrarModal;
 
     const body = document.querySelector("body");
+    body.classList.add("overflow-hidden");
     body.appendChild(modal);
 }
 
-function cerrarModal(){
+function cerrarModal() {
     const modal = document.querySelector(".modal");
-    modal?.remove();
+    modal.classList.add("fade-out");
+    setTimeout(() => {
+        modal?.remove();
+    }, 500);
+
+     const body = document.querySelector("body");
+    body.classList.remove("overflow-hidden");
+
 }
