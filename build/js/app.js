4f8888dae76610e7e542e7d6f7795b67f07508a7
Angel Orozco <angelalfonsool20@gmail.com>
2025-08-01 22:10:30 -0600

    Agregando codigo js para mostrar imagen y para eliminarlo de la pantalla.

--- build/js/app.js
+++ build/js/app.js
@@ -10,5 +10,24 @@ function crearGaleria(){
         img.alt = "Imagen Musica";
         galeria.appendChild(img);
 
+        img.onclick = function(){
+            mostrarImagen(i);
+        }
+
     }
+}
+
+function mostrarImagen(i){
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
+
+    modal.onclick = cerrarModal;
+
+    const body = document.querySelector("body");
+    body.appendChild(modal);
+}
+
+function cerrarModal(){
+    const modal = document.querySelector(".modal");
+    modal?.remove();
 }
